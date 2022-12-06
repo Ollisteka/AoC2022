e67b2257770ce4f796aed3338bef827c5afa53bc
Жукова Ольга Михайловна <zhukova.o.m@skbkontur.ru>
2022-12-06 10:34:48 +0500

    day 06

---
+++ src/day-06/part1.mjs
@@ -0,0 +1,16 @@
+import { readToString } from "./stdin.mjs";
+
+const MARKER_LENGTH = 4;
+
+async function solve() {
+  const input = await readToString().then(res => res.trim('\r\n'));
+
+  for (let i = 0; i < input.length; i++) {
+    const possibleMarker = input.substring(i, i + MARKER_LENGTH);
+    if (new Set(possibleMarker.split('')).size === MARKER_LENGTH) {
+      return i + MARKER_LENGTH;
+    }    
+  }
+}
+
+solve().then(console.log);
